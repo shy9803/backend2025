3b434112a0c3286912c834562a891c05fdbf1f84
shy9803 <127914332+shy9803@users.noreply.github.com>
2025-06-05 11:51:49 +0900

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const connection = mysql.createConnection({
   host:'database',
   user:'root',
   password:'1234',
-  database:'database'
+  database:'kdt'
 });
 
 //2. MYSQL DB접속시 오류가 나면 에러 출력하기, 성공하면 '성공'표시하기
