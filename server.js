cf2390ae83bb5f596ad4eacd4e13a43612d314c2
shy9803 <127914332+shy9803@users.noreply.github.com>
2025-06-23 18:23:23 +0900

    Update server.js

--- server.js
+++ server.js
@@ -779,29 +779,29 @@ app.delete('/api/cart', authenticateToken, (req, res) => {
 app.post('/api/cart', authenticateToken, (req, res) => {
   const { product_id } = req.body;
   // 상품 상세 정보 조회
-  const productSql = `SELECT title, brand, \`condition\`, price, trade_type, region, image_main, shipping_fee FROM green_products WHERE id = ?`;
+  const productSql = 
+    'SELECT title, brand, \`condition\`, price, trade_type, region, image_main, shipping_fee FROM green_products WHERE id = ?';
   connectionGM.query(productSql, [product_id], (productErr, productResults) => {
-    if (productErr) return res.status(500).json({ message: '상품 조회 오류' });
-    if (!productResults.length) return res.status(404).json({ message: '상품 없음' });
+    if (productErr) return res.status(500).json({ error: '상품 조회 오류' });
+    if (!productResults.length) return res.status(404).json({ error: '상품 없음' });
 
     const product = productResults[0];
 
     // 장바구니 중복 확인
     const checkSql = 'SELECT * FROM green_cart WHERE user_id = ? AND product_id = ?';
     connectionGM.query(checkSql, [req.user.id, product_id], (checkErr, checkResults) => {
-      if (checkErr) return res.status(500).json({ message: 'DB 오류' });
+      if (checkErr) return res.status(500).json({ error: 'DB 오류' });
 
       if (checkResults.length) {
         return res.status(400).json({ message: '이미 장바구니에 있는 상품입니다.' });
       }
 
       // 장바구니에 상품 추가
-      const insertSql = `
-        INSERT INTO green_cart
-        (user_id, product_id, title, brand, \`condition\`, shipping_fee, price, trade_type, region, image_main, added_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())`;
+      const insertSql = 
+        'INSERT INTO green_cart (user_id, product_id, title, brand, \`condition\`, shipping_fee, price, trade_type, region, image_main, added_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, NOW())';
 
       const params = [
-        req.user.id, product_id, product.title, product.brand, product.kind, product.condition, product.shipping_fee, product.price, product.trade_type, product.region, product.image_main
+        req.user.id, product_id, product.title, product.brand, product.condition, product.shipping_fee, product.price, product.trade_type, product.region, product.image_main
       ];
 
       connectionGM.query(insertSql, params, (insertErr) => {
